7181070cef216c9da9d12eb2d78af672b73cffa5
andersongilvan <torresdinho8@gmail.com>
2024-08-05 12:26:02 -0300

    Adiciona entidade JPA Usuario, cria tabela usuario, e configura autenticacao com criptografia Bcrypt e JWT

---
+++ src/main/java/med/voll/api/controller/AutenticacaoController.java
@@ -0,0 +1,39 @@
+package med.voll.api.controller;
+
+import jakarta.validation.Valid;
+import med.voll.api.domain.usuario.DtoUsuario;
+import med.voll.api.infra.security.DtoTokenJwt;
+import med.voll.api.infra.security.TokenService;
+import med.voll.api.models.usuario.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+public class AutenticacaoController {
+
+    @Autowired
+    private AuthenticationManager manager;
+
+    @Autowired
+    private TokenService tokenService;
+
+    @PostMapping
+    public ResponseEntity efetuarLogin(@RequestBody @Valid DtoUsuario dados) {
+        var autheticationToken = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
+
+       var authentication = manager.authenticate(autheticationToken);
+       var tokenJWT = tokenService.gerarToken((Usuario) authentication.getPrincipal());
+       var dtoTokenJwt = new DtoTokenJwt(tokenJWT);
+      return ResponseEntity.ok(dtoTokenJwt);
+
+    }
+
+
+}
